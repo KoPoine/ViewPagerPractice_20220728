bdf006930528c65ba4982d44286f57eb4b0bb8c4
김민철 <kmc901206@gmail.com>
2022-07-28 15:59:12 +0900

    ViewPager 사용될 Adapter 제작

---
+++ app/src/main/java/com/neppplus/viewpagerpractice_20220728/adapters/MainViewPagerAdapter.kt
@@ -0,0 +1,26 @@
+package com.neppplus.viewpagerpractice_20220728.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.neppplus.viewpagerpractice_20220728.fragments.BirthYearFragment
+import com.neppplus.viewpagerpractice_20220728.fragments.NameFragment
+import com.neppplus.viewpagerpractice_20220728.fragments.PhoneFragment
+
+class MainViewPagerAdapter(
+    fm : FragmentManager
+) : FragmentPagerAdapter(fm) {
+//    몇 장짜리 ViewPager인지?
+    override fun getCount(): Int {
+        return 3
+    }
+
+//    각 페이지에는 뭐 보여줄건지?
+    override fun getItem(position: Int): Fragment {
+        return when (position) {
+            0 -> NameFragment()
+            1 -> BirthYearFragment()
+            else -> PhoneFragment()
+        }
+    }
+}
